759611b8107eef927ba4f67d8e9dd971ef5c117d
KARTHIK R V <karthikrv1107@gmail.com>
2026-03-03 19:45:47 +0530

    Update db.php

--- Blogpost/db.php
+++ Blogpost/db.php
@@ -1,9 +1,9 @@
 <?php
-$server = "db.szngjfzvznhrrsmwvbhw.supabase.co";
-$port = "5432";
-$dbname = "postgres";
-$user = "postgres";
-$pass = " fDhiuTMv1iV2sLlm";
+$server = "";
+$port = "";
+$dbname = "";
+$user = "";
+$pass = "";
 $conn = pg_connect(
     "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require"
 );
